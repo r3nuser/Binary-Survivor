1b32b33941a9eb9154440a779e7c1cd16f00f2a2
Renan M. Gomes <renan_battlenet@hotmail.com>
2018-02-26 03:40:35 -0300

    Good

--- Game/script.js
+++ Game/script.js
@@ -1,20 +1,31 @@
+
 var elemento;
-var text;
+
 function Inicializa(){
 	elemento = document.getElementById("text-lines");
-}
-
-function espaces(){
-	console.log("xupa")
-	elemento.focus();
-	text = elemento.value;
-	text+="    "
-	elemento.value = text;
+}	
+function moveToFrontWhiteSpace(text, index, value){
+	var formatted;
+	formatted = text.substring(0,index)
+	for(var i = 0 ; i < value ; i++){
+		formatted+=' ';
+	}
+	formatted+= text.substr(index);	
+	console.log(formatted);
 	console.log(elemento);
-var teclaEsquerda = jQuery.Event("keypress");
-teclaEsquerda.ctrlKey = false;
-teclaEsquerda.which = 21; //Código da tecla - seta esquerda
-
-$("botao").trigger(teclaEsquerda);
-	
+	return formatted;
 }
+function espaces(){
+	var cursorPosition = $('#text-lines').prop("selectionStart");
+	elemento.value=moveToFrontWhiteSpace(elemento.value, cursorPosition, 4);
+	elemento.focus();
+	elemento.selectionEnd = cursorPosition + 4;
+	elemento.selectionStart = cursorPosition + 4;
+	console.log(cursorPosition);
+}	
+
+
+/*var teclaEsquerda = jQuery.Event("keypress");
+	teclaEsquerda.ctrlKey = false;
+	teclaEsquerda.which = 21; //Código da tecla - seta esquerda
+	$("botao").trigger(teclaEsquerda);*/
